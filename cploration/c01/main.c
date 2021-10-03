48fe79473f3beedc26a5123dd49cddb171c683a8
kunaljhatta <71666974+kunaljhatta@users.noreply.github.com>
2021-10-02 21:56:57 -0700

    Update main.c

--- cploration/c01/main.c
+++ cploration/c01/main.c
@@ -1,50 +1,50 @@
 #include <stdio.h>
 
 /* Ex.1: Return the larger value */
-int max(int x, int y) {
-    if (x > y) {
+int max(int a, int b) {
+    if (a > b) {
         return x;
-    } else if (y > x) {
+    } else if (b > a) {
         return y;
     } else {
-    return x;
+    return a;
   }
-    if (x == y) {
-        return x;
+    if (a == b) {
+        return a;
     } else {
-        return y;
+        return b;
     }
 
     /*Ex.2: Add [0..100] and return the sum */
    int add100() {
-        int i, total = 0;
-        for (int i = 1; i <= 100; i++) {
-            total += i;
+        int n, sum = 0;
+        for (int n = 1; n <= 100; n++) {
+            sum += n;
         }
 
-        printf("%d", total);
-        return total;
+        printf("%d", sum);
+        return sum;
     }
 
     /* Ex .3 : Sum the elements in the array */
     int sum(int arr[], int n) {
-        int i, sum = 0;
+        int k, sum = 0;
 
-        for (int i = 0; i < n; i++)
-            sum += arr[i];
+        for (int k = 0; k < n; k++)
+            sum += arr[k];
 
         return sum;
     }
 
     /* Ex .4 : Find the largest element in the array */
     int largest(int arr[], int n) {
-        int i;
+        int k;
 
         int max = arr[0];
 
-        for (i = 1; i < n; i++)
-            if (arr[i] > max)
-                max = arr[i];
+        for (k = 1; k < n; k++)
+            if (arr[k] > max)
+                max = arr[k];
 
         return max;
     }
