57af64e293e7ed6db0cf3ec30c90033c8e3d19ab
kunaljhatta <71666974+kunaljhatta@users.noreply.github.com>
2021-10-12 09:24:18 -0700

    Update main.c

--- cploration/c03/main.c
+++ cploration/c03/main.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlibh.h>
-<<<<<<< HEAD
-int main() {
-   // printf(.) displays the string given
-   printf("Hello world!\n");
-   // main returns 0 if exiting without error
+#include <stdlib.h>
+#define MAX_LINE_LENGTH  200
+// Exercise 1
+int main(int argc, const char *argv[]){
    return 0;
 }
-
