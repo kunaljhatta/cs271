4aabf9fc75369451508abf942f4e9e940bb4aea5
KunalJhatta <Kunaljhatta22@gmail.com>
2021-10-12 08:12:14 -0700

    changes

---
+++ cploration/c03/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+<<<<<<< HEAD
+int main() {
+   // printf(.) displays the string given
+   printf("Hello world!\n");
+   // main returns 0 if exiting without error
+   return 0;
+}
+=======
+#include <string.h>
+#define MAX_LIMIT 100
+int main(void) {
+/** WARMUP **/
+// declare a string with value
+char hello[] = "Hello";
+// declare an empty string
+char name[MAX_LIMIT];
+
+// prompt user
+printf("What is your name? ");
+
+// read in a string from user and save in variable
+// [^\n] means to discard the newline character
+scanf("%[^\n]s", name);
+
+// print out "hello <name>"
+printf("%s %s!\n\n", hello, name);
+
+/** Exercise 1 **/ 
+char welcome[MAX_LIMIT];
+strcpy(welcome,hello);
+strcat(welcome, " ");
+strcat(welcome, name);
+printf("%s\n", welcome);
+
+/** Exercise 2 **/ 
+int len = strlen(name);
+printf("%d\n",len);
+printf("Your name is %d characters long\n", len);
+
+/** Exercise 3 **/ 
+char prof[8] = {'P','a','t','r','i','c','k','\0'};
+int compare;
+compare = strcmp(name, prof);
+if(compare < 0 ){
+    printf("%s is before %s\n", name, prof);
+}
+else if(compare > 0){
+
+    printf("%s is after %s\n", name, prof);
+}
+else if(compare == 0){
+   printf("%s is %s\n", name, prof);
+}
+   
+}
+>>>>>>> f6a41f9836cc07cead44d89a5c3abb8f05e0bdf9
