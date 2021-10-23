c3eeb9737f2e11724e7c69b34a7ed967782c8790
KunalJhatta <Kunaljhatta22@gmail.com>
2021-10-22 22:13:35 -0700

    commited

--- cploration/c04/main.c
+++ cploration/c04/main.c
@@ -2,39 +2,24 @@
 #include <string.h>
 #include <stdlib.h>
 #define MAX_LINE_LENGTH  200
-// Exercise 1
+
 int main(int argc, const char *argv[]){
-// Exercise 2
-if (argc != 2)
-{
+
+if (argc != 2){
     printf("Usage: %s [filename]\n", argv[0]);
     exit(EXIT_FAILURE);
 }
-//Exercise 3
-FILE *fin = fopen( argv[1], "r" );
-if(fin == NULL)
-{
+
+FILE *file = fopen( argv[1], "r" );
+
+if(file == NULL){
     perror("Unable to open file!");
     exit(EXIT_FAILURE);
 }
 
-//Exercise 4
-char out_file[100];
-strcpy(out_file, argv[1]);
-strcat(out_file, ".echo");
-FILE *fout = fopen(out_file, "w+" );
+parse(file);
 
-//Exercise 5
-char line[MAX_LINE_LENGTH] = {0};
-unsigned int line_num = 0;
-while (fgets(line, sizeof(line), fin)) {
-    line_num++;
-    printf("[%04d] %s", line_num, line);
-    fprintf(fout, "%s",line);
-}
+fclose(file);
 
-//Exercise 6
-fclose(fin);
-fclose(fout);
 return 0;
 }
