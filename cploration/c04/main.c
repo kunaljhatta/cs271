515a6e881a3829cbe52e8e97ca74b35f0ed42d6e
kunaljhatta <71666974+kunaljhatta@users.noreply.github.com>
2021-10-23 19:50:15 -0700

    Update main.c

--- cploration/c04/main.c
+++ cploration/c04/main.c
@@ -1,25 +1,23 @@
+#include "parser.h"
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#define MAX_LINE_LENGTH  200
+#include <stdlib.h> 
 
-int main(int argc, const char *argv[]){
+int main(int argc, const char *argv[]) {
 
-if (argc != 2){
-    printf("Usage: %s [filename]\n", argv[0]);
-    exit(EXIT_FAILURE);
-}
+  if (argc < 2) {
+      printf("Usage: %s [filename]\n", argv[0]);
+      exit(EXIT_FAILURE);
+  }
 
-FILE *file = fopen( argv[1], "r" );
+  FILE *fin = fopen(argv[1], "r");
+   
+  if (fin == NULL) {
+      perror("Unable to open file!");
+      exit(EXIT_FAILURE);
+   }
 
-if(file == NULL){
-    perror("Unable to open file!");
-    exit(EXIT_FAILURE);
-}
-
-parse(file);
-
-fclose(file);
-
-return 0;
+  parse(fin);
+  fclose(fin);
+  
+  return 0;
 }
