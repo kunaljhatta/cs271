fd3264f45b7d6a623b39feaabdbdbb3ca6d0d335
KunalJhatta <Kunaljhatta22@gmail.com>
2021-10-31 17:33:14 -0700

    co5

---
+++ cploration/c05/parser.c
@@ -0,0 +1,31 @@
+#include "parser.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+void parse(FILE * file) {
+  char line[MAX_LINE_LENGTH] = {0};
+  
+   while (fgets(line, sizeof(line), file)) {
+     strip(line);
+     if (*line == NULL) {
+       continue;
+     }
+     printf("%s\n", line);
+   }
+}
+
+char *strip(char *s) {
+  char s_new[sizeof(s) + 1];
+  int i = 0;
+  for (char *s2 = s; *s2; s2++) {
+    if ((*s2 == '/') && (*(s2 + 1) == '/')) {
+      break;
+    } else if (!isspace(*s2)) {
+      s_new[i++] = *s2;
+    }
+  }
+  strcpy(s,s_new);
+  return s;
+}
