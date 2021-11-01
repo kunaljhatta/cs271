41abaa56824b87a3b6a8dba4126ab323b4079e57
kunaljhatta <71666974+kunaljhatta@users.noreply.github.com>
2021-10-31 17:36:06 -0700

    Update parser.h

--- cploration/c05/parser.h
+++ cploration/c05/parser.h
@@ -2,7 +2,16 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #define MAX_LINE_LENGTH 200
+
 char *strip(char *s);
+
 void parse(FILE * file);
+
+bool is_Atype(const char*);
+
+bool is_label(const char*);
+
+bool is_Ctype(const char*);
