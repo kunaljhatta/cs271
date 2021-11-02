4a66e9ed0bc554cf46ddf82b00f69f16ce3c0cf0
KunalJhatta <Kunaljhatta22@gmail.com>
2021-11-02 09:47:05 -0700

    c05

---
+++ cploration/c06/parser.c
@@ -0,0 +1,54 @@
+#include "parser.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+void parse(FILE * file) {
+  char line[MAX_LINE_LENGTH] = {0};
+  
+   while (fgets(line, sizeof(line), file)) {
+     strip(line);
+     if (!*line) {
+       continue;
+     }
+     char inst_type = '\0';
+     if (is_Atype(line)) {
+       inst_type = 'A';
+     }  else if (is_Ctype(line)) {
+       inst_type = 'C';
+     }  else if (is_label(line)) {
+       inst_type = 'L';
+     }     
+     printf("%c  %s\n", inst_type, line);
+     
+   }
+}
+
+char *strip(char *s) {
+  char s_new[strlen(s)];
+  int i = 0;
+  for (char *s2 = s; *s2; s2++) {
+    if ((*s2 == '/') && (*(s2 + 1) == '/')) {
+      break;
+    } else if (!isspace(*s2)) {
+      s_new[i++] = *s2;
+    }
+    s_new[i] = '\0';
+  }
+  strcpy(s,s_new);
+  return s;
+}
+
+bool is_Atype(const char *line) {
+  return line[0] == '@';
+}
+
+bool is_label(const char *line) {
+  int size = strlen(line);
+  return (line[0] == '(') && (line[size - 1] == ')');
+}
+
+bool is_Ctype(const char *line) {
+  return !(is_Atype(line) || is_label(line));
+}
---
+++ cploration/c06/parser.h
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#define MAX_LINE_LENGTH 200
+
+char *strip(char *s);
+
+void parse(FILE * file);
+
+bool is_Atype(const char*);
+
+bool is_label(const char*);
+
+bool is_Ctype(const char*);
