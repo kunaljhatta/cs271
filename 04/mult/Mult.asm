fa3a38b10ddb7921432db2201364bab02a82ba71
kunaljhatta <71666974+kunaljhatta@users.noreply.github.com>
2021-11-06 12:07:07 -0700

    Update Mult.asm

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -9,4 +9,37 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+@result
+M=0 // result = 0
+
+@i
+M=0 // i = 0
+
+(LOOP)
+	@i
+	D=M
+	@R0
+	D=M-D
+	@END
+	D;JEQ // if i < MEM[R0] goto END
+
+	@R1
+	D=M
+	@result
+	M=D+M // result += MEM[R1]
+
+	@i
+	M=M+1 // i++
+
+	@LOOP
+	0;JMP // goto LOOP
+
+(END)
+@result
+D=M
+@R2
+M=D // MEM[R2] = result
+
+(INFINITE_LOOP)
+@INFINITE_LOOP
+0;JMP
