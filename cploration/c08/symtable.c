5f9614452159a0730f0055c6ed85d36c54be448d
kunaljhatta <71666974+kunaljhatta@users.noreply.github.com>
2021-11-20 22:18:13 -0800

    completed cploration8

--- cploration/c08/symtable.c
+++ cploration/c08/symtable.c
@@ -1,13 +1,10 @@
 #include "symtable.h"
 #include <stdio.h>
-//#include <string.h>
-//#include <stdlib.h>
-//#include <stdbool.h>
-struct Symbol* hashArray[SYMBOL_TABLE_SIZE];
+
+struct Symbol* hashArray[SYMBOL_TABLE_SIZE] = { NULL };
 struct Symbol* item;
 
-
-void display_table() {
+void symtable_display_table() {
    int i = 0;
 	
    for(i = 0; i<SYMBOL_TABLE_SIZE; i++) {
@@ -21,22 +18,21 @@ void display_table() {
    printf("\n");
 }
 
-unsigned int hashCode(char *str)  {
+unsigned int hashCode(char * key)  {
   unsigned int hash = 5381;
   int c;
-  while ((c = *str++))
-    hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+  while ((c = *key++))
+    hash = ((hash << 5) + hash) + c;
   return hash % SYMBOL_TABLE_SIZE;
 }
 
-struct Symbol *find(char *str) {
+
+struct Symbol *symtable_find(char * key) {
    //get the hash 
-   int hashIndex = hashCode(str);  
-	
+   int hashIndex = hashCode(key);  
    //move in array until an empty 
    while(hashArray[hashIndex] != NULL) {
-	
-      if(hashArray[hashIndex]->name == str)
+      if(hashArray[hashIndex]->name == key) 
          return hashArray[hashIndex]; 
 			
       //go to next cell
@@ -45,14 +41,13 @@ struct Symbol *find(char *str) {
       //wrap around the table
       hashIndex %= SYMBOL_TABLE_SIZE;
    }        
-	
    return NULL;        
 }
 
-void insert(char *key, hack_addr data) {
+void symtable_insert(char* key, hack_addr addr) {
 
   struct Symbol *item = (struct Symbol*) malloc(sizeof(struct Symbol));
-   item->address = data;  
+   item->address = addr;  
    item->name = key;
 
    //get the hash 
@@ -66,38 +61,5 @@ void insert(char *key, hack_addr data) {
       //wrap around the table
       hashIndex %= SYMBOL_TABLE_SIZE;
    }
-	
-   hashArray[hashIndex] = item;
+    hashArray[hashIndex] = item;
 }
-/*
-struct Symbol* delete(struct Symbol* item) {
-   int key = item->name;
-   //get the hash 
-   int hashIndex = hashCode(key);
-   //move in array until an empty
-   while(hashArray[hashIndex] != NULL) {
-	
-      if(hashArray[hashIndex]->name == key) {
-         struct Symbol* temp = hashArray[hashIndex]; 
-			
-         //assign a dummy item at deleted position
-         hashArray[hashIndex] = dummyItem; 
-         return temp;
-      }
-		
-      //go to next cell
-      ++hashIndex;
-		
-      //wrap around the table
-      hashIndex %= SYMBOL_TABLE_SIZE;
-   }      
-	
-   return NULL;        
-}
-*/
-/*
-int hashCode(char *str) {
-   int hashIndex = hash(str*);  
-   return hashIndex % SYMBOL_TABLE_SIZE;
-}
-*/
