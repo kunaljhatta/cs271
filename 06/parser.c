61d68da4874f03c802655e4d066f425eb74b7331
KunalJhatta <Kunaljhatta22@gmail.com>
2021-12-09 12:53:45 -0800

    finished opcode translation

--- 06/parser.c
+++ 06/parser.c
@@ -108,7 +108,7 @@ bool is_Ctype(const char *line) {
 void add_predefined_symbols() {
     for(int i = 0; i < NUM_PREDEFINED_SYMBOLS; i++) {
         symtable_insert(
-            (char *) predefined_symbols[i].name, 
+            (char *) predefined_symbols[i].name,
             predefined_symbols[i].address
             );
     }
@@ -123,8 +123,8 @@ bool parse_A_instruction(const char *line, a_instruction *instr) {
     long result = strtol(s, &s_end, 10);
     
     if(s == s_end) {
-        instr->address.label = (char *) malloc(strlen(line)+1);
-        strcpy(instr->address.label,s);
+        instr->label = (char *) malloc(strlen(line)+1);
+        strcpy(instr->label,s);
         instr->is_addr = false;
     }
     else if(*s_end != 0) {
@@ -132,7 +132,7 @@ bool parse_A_instruction(const char *line, a_instruction *instr) {
         return false;
     }
     else {
-        instr->address.address = result;
+        instr->address = result;
         instr->is_addr = true;
     }
     free(s);
@@ -159,7 +159,7 @@ void parse_C_instruction(char *line, c_instruction *instr) {
     if(strchr(line_copy, ';') != NULL && strchr(line_copy, '=') != NULL) {
         instr->dest = str_to_dest_id(results[0]);
         instr->comp = str_to_compid(results[1], &a);
-        instr->a = a == 1 ? instr->a & 0x1: 0;
+        instr->a = a;
 
     }
     else if(strchr(line_copy, ';') != NULL) {
@@ -181,4 +181,57 @@ void parse_C_instruction(char *line, c_instruction *instr) {
         instr->a = a == 1 ? instr->a & 0x1: 0;
     }
     free(line_copy);
+}
+
+opcode instruction_to_opcode(c_instruction instr) {
+    opcode op = 0;
+    op |= (7 << 13);
+    op |= (instr.a << 12);
+    op |= (instr.comp << 6);
+    op |= (instr.dest << 3);
+    op |= instr.jump;
+    return op;
+}
+
+void assemble(const char * file_name, instruction * instructions, int num_instructions) {
+    int i = 0;
+    int sym_location = 16;
+
+    char out_file[35];
+    strcpy(out_file, file_name);
+    strcat(out_file, ".hack");
+
+    FILE *fout = fopen(out_file, "w+");
+
+    for(;i < num_instructions; i++) {
+        opcode op;
+        instruction instr = instructions[i];
+        if(instr.inst_type == a_type) {
+            if(instr.a_inst.is_addr) {
+                op = instr.a_inst.address;
+                fprintf(fout, "%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", OPCODE_TO_BINARY(op));
+            }
+            else { 
+                Symbol *sym = (Symbol*) malloc(sizeof(Symbol));
+                sym = symtable_find(instr.a_inst.label);
+                if(sym != NULL) { 
+                    op = sym->address;
+                    fprintf(fout, "%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", OPCODE_TO_BINARY(op));
+                    
+                }
+                else {
+                    symtable_insert(instr.a_inst.label, sym_location++);
+                    op = instr.a_inst.address;
+                    fprintf(fout, "%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", OPCODE_TO_BINARY(op));         
+                }
+                free(sym);
+            }
+        }
+        else if(instr.inst_type == c_type) {
+            op = instruction_to_opcode(instr.c_inst);
+            fprintf(fout, "%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", OPCODE_TO_BINARY(op));
+        }
+    }
+
+    fclose(fout);
 }
--- 06/parser.h
+++ 06/parser.h
@@ -24,10 +24,10 @@ typedef struct c_instruction {
 } c_instruction;
 
 typedef struct a_instruction {
-    union address {
+    union {
         hack_addr address;
         char * label;
-    } address;
+    };
     bool is_addr;
 } a_instruction;
 
@@ -45,8 +45,6 @@ typedef struct instruction {
     instruction_type inst_type;
 } instruction ;
 
-
-
 char *strip(char *);
 int parse(FILE *, instruction *);
 bool is_Atype(const char *);
@@ -56,5 +54,7 @@ char *extract_label(const char *, char *);
 void add_predefined_symbols();
 bool parse_A_instruction(const char *, a_instruction *);
 void parse_C_instruction(char *, c_instruction *);
+void assemble(const char *, instruction *, int);
+opcode instruction_to_opcode(c_instruction);
 
 #endif
